0456d48191adadaf20fa9f989d37ec36df00bc0c
Sabir222 <mrsabir4@gmail.com>
2023-07-26 00:25:59 +0100

    Adding Nav Bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,41 +33,43 @@ const Navbar = () => {
     <>
       <MobileNav visible={isVisible} Click={handleClick} />
       <nav
-        className={`max-w-[1400px]  ease-in-out duration-500  px-[32px] z-20 fixed items-center justify-between flex top-0 left-0 right-0 h-16  mx-auto ${
+        className={`  ease-in-out duration-500  px-[32px] z-20 fixed  top-0 left-0 right-0 h-16   ${
           scrolled ? "bg-black/40 backdrop-blur-md " : "bg-transparent "
         }  `}
       >
-        <div className="">
-          <Link href="/">
-            <div className="cursor-pointer top-4 left-4">
-              <Image
-                src={"/tree.png"}
-                alt="logo"
-                width={40}
-                height={40}
-                style={{ width: 40, height: 40 }}
-              />
+        <div className="max-w-[1400px] mx-auto items-center justify-between flex pt-2">
+          <div className="">
+            <Link href="/">
+              <div className="cursor-pointer top-4 left-4">
+                <Image
+                  src={"/tree.png"}
+                  alt="logo"
+                  width={40}
+                  height={40}
+                  style={{ width: 40, height: 40 }}
+                />
+              </div>
+            </Link>
+          </div>
+          <div>
+            <ul className="hidden gap-10 text-white md:flex">
+              <li>Work</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex gap-2">
+              <button
+                className="flex items-center justify-center w-8 h-8 rounded-full md:hidden bg-zinc-100 bg-opacity-10 top-4 right-4"
+                onClick={handleClick}
+              >
+                <AiOutlineMenu className="text-white" />
+              </button>
+              <button className="flex items-center justify-center w-8 h-8 rounded-full bg-zinc-100 bg-opacity-10 top-4 right-4">
+                <IoSunnyOutline className="text-white" />
+              </button>
             </div>
-          </Link>
-        </div>
-        <div>
-          <ul className="hidden gap-10 text-white md:flex">
-            <li>Work</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        <div>
-          <div className="flex gap-2">
-            <button
-              className="flex items-center justify-center w-8 h-8 rounded-full md:hidden bg-zinc-100 bg-opacity-10 top-4 right-4"
-              onClick={handleClick}
-            >
-              <AiOutlineMenu className="text-white" />
-            </button>
-            <button className="flex items-center justify-center w-8 h-8 rounded-full bg-zinc-100 bg-opacity-10 top-4 right-4">
-              <IoSunnyOutline className="text-white" />
-            </button>
           </div>
         </div>
       </nav>
