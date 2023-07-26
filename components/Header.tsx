5994358aac9acf630aecc89e9131c31c37c4a959
Sabir222 <mrsabir4@gmail.com>
2023-07-26 01:57:07 +0100

    updating nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ interface headerProps {
 }
 const Header = ({ firstp, secondp }: headerProps) => {
   return (
-    <div className="h-[50vh] p-6 relative  animation-div flex justify-start md:justify-center items-center ">
+    <div className="h-[100vh] sm:h-[50vh] p-6 relative  animation-div flex justify-start md:justify-center items-center ">
       <div className="p-5 bg-slate-100 bg-opacity-10 max-w-[1400px] md:rounded-xl">
         <Image src={"/logo.png"} alt="logo" width={30} height={30} />
         <p className="mb-4 text-2xl font-bold text-white">{firstp}</p>
