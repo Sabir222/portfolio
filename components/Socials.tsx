9070be23ab6d11145a6cfe7373b0284443a60fff
Sabir222 <mrsabir4@gmail.com>
2023-10-25 20:08:43 +0100

    pdf file

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,7 +1,6 @@
 import { BsLinkedin, BsGithub, BsCloudDownload } from "react-icons/bs";
 import { RiTwitterXLine } from "react-icons/ri";
 const Socials = () => {
-  const slides = ["A", "B", "C", "D"];
   return (
     <section className="dark:bg-[#EADBC8] bg-[#EEEEEE]  text-black ">
       <div className=" max-w-[1400px] mx-auto px-[32px] pb-[1200px] relative md:pb-[250px]">
@@ -13,10 +12,11 @@ const Socials = () => {
             <div className=" dark:text-white">
               <BsCloudDownload size={70} />
             </div>
-            <a></a>
-            <button className="w-[100px] py-2 text-white rounded animation-div3">
-              Download
-            </button>
+            <a download="My Resume.pdf" href="My Resume.pdf" target="_blank">
+              <button className="w-[100px] py-2 text-white rounded animation-div3">
+                Download
+              </button>
+            </a>
           </div>
           <div className="h-[300px]  ease-in-out duration-300 md:w-[300px] bg-[#fbfdfb] dark:bg-zinc-900 rounded-lg flex flex-col items-center justify-center gap-4 ">
             <h1 className="text-xl font-semibold dark:text-white">
