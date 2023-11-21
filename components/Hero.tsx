d11a08361d405d94e5b291ddb6c028f3e74efd7b
Sabir222 <mrsabir4@gmail.com>
2023-11-21 10:38:13 +0100

    typo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Header from "./Header";
-import Projects from "./Projects";
 
 const Hero = () => {
   //
@@ -9,12 +8,10 @@ const Hero = () => {
       <div className="flex flex-col ">
         <Header
           contact=""
-          firstp="Hello, I'm Sabir Welcome to my Webiste"
+          firstp="Hello, I'm Sabir Welcome to my Website"
           secondp="Self-taught web developer with a passion for creativity and endless
           possibilities. Happy to collaborate as a Front-end web developer."
         />
-        {/* <div className="h-full duration-300 dark:bg-[#EADBC8] bg-[#EEEEEE] pb-[80px]">
-        </div> */}
       </div>
     </div>
   );
