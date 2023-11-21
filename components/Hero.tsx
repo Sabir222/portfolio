669cdc01ba744c6a358a6d923c7b282ac8cb4a22
Sabir222 <mrsabir4@gmail.com>
2023-11-21 15:02:39 +0100

    just do it

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="flex flex-col ">
         <Header
           contact=""
-          firstp="Hello, I'm Sabir Welcome to my Website"
+          firstp="Hello, I'm Sabir Koutabi Welcome to my Website"
           secondp="Self-taught web developer with a passion for creativity and endless
           possibilities. Happy to collaborate as a Front-end web developer."
         />
