49704b51c33d42d6aa8ef5dfc6bb84eb85ad291b
Sabir222 <mrsabir4@gmail.com>
2023-11-27 22:24:47 +0100

    changing description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,9 +8,7 @@ const Hero = () => {
       <div className="flex flex-col ">
         <Header
           contact=""
-          firstp="Hello, I'm Sabir Koutabi Welcome to my Website"
-          secondp="Self-taught web developer with a passion for creativity and endless
-          possibilities. Happy to collaborate as a Front-end web developer."
+          firstp="Hello, I'm Sabir Koutabi, and after immersing myself in the world of finance and banking for four years, I've taken a bold leap into the realm of programming. Motivated by a deep passion for web development, I've paved my own journey as a self-taught front-end developer. I'm excited to bring my unique perspective to the digital landscape, aiming to enhance and optimize digital experiences with my distinctive approach."
         />
       </div>
     </div>
