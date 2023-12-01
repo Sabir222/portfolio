0281980418cf0ee514b8197002758c008a488444
Sabir222 <mrsabir4@gmail.com>
2023-12-01 01:06:21 +0100

    code cleaning

--- app/studies/page.tsx
+++ app/studies/page.tsx
@@ -6,14 +6,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import TheModal from "@/components/StudyModal";
 import { Metadata } from "next";
-import { FaCopyright } from "react-icons/fa";
-import {
-  BiLogoTypescript,
-  BiLogoJavascript,
-  BiLogoPython,
-} from "react-icons/bi";
-import { DiHtml5, DiCode } from "react-icons/di";
-import { FaTerminal, FaRust } from "react-icons/fa";
+import GithubCard from "@/components/GithubCard";
 
 export const metadata: Metadata = {
   title: "Studies",
@@ -48,71 +41,14 @@ const page = async () => {
               {repos.map((repo: DataProps, index: any) => {
                 const shortenedCreatedAt = repo.created_at.slice(0, 10);
                 return (
-                  <a href={repo.html_url} target="_blank" key={index}>
-                    <div>
-                      <div className="dark:transparent cursor-pointer  mb-5 md:mb-0  h-[300px] flex flex-col ring-gray-600  ring-1  shadow-md p-7 ">
-                        <div className="h-[30%] flex align-center  gap-3">
-                          <div>
-                            {repo.language === "Python" ? (
-                              <BiLogoPython size={50} />
-                            ) : repo.language === "Shell" ? (
-                              <FaTerminal size={50} />
-                            ) : repo.language === "JavaScript" ? (
-                              <BiLogoJavascript size={50} />
-                            ) : repo.language === "TypeScript" ? (
-                              <BiLogoTypescript size={50} />
-                            ) : repo.language === "HTML" ? (
-                              <DiHtml5 size={50} />
-                            ) : repo.language === "C" ? (
-                              <FaCopyright size={50} />
-                            ) : (
-                              <DiCode size={50} />
-                            )}
-                          </div>
-                          <div className="flex flex-col">
-                            <div className="relative text-lg font-semibold ">
-                              <div>
-                                <span
-                                  className={`${
-                                    repo.language === "Python"
-                                      ? "underline-python"
-                                      : repo.language === "Shell"
-                                      ? "underline-shell"
-                                      : repo.language === "JavaScript"
-                                      ? "underline-javascript"
-                                      : repo.language === "TypeScript"
-                                      ? "underline-typescript"
-                                      : repo.language === "HTML"
-                                      ? "underline-html"
-                                      : repo.language === "C"
-                                      ? "underline-c"
-                                      : "highlight"
-                                  } ${repo.language ?? "underline-noLang"}`}
-                                >
-                                  {repo.language === "C"
-                                    ? "C Language"
-                                    : repo.language ?? "No language detected"}
-                                </span>
-                              </div>
-                            </div>
-                            <p className="line-clamp-1 ">{repo.name}</p>
-                          </div>
-                        </div>
-                        <div className="h-[70%] pt-2 pl-1 text-sm flex gap-1 flex-col">
-                          <p className="text-gray-600 ">{"<h3>"}</p>
-                          <div className="flex gap-3 ">
-                            <div className="">
-                              <div className="h-full ml-4 border-l border-gray-600 border-solid "></div>
-                            </div>
-                            <p className="h-full line-clamp-3">
-                              {repo.description}
-                            </p>
-                          </div>
-                          <p className="text-gray-600 ">{"</h3>"}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
+                  <GithubCard
+                    height="300px"
+                    key={index}
+                    repoUrl={repo.html_url}
+                    repoDesc={repo.description}
+                    repoLang={repo.language}
+                    repoName={repo.name}
+                  />
                 );
               })}
             </div>
--- components/Github.tsx
+++ components/Github.tsx
@@ -2,14 +2,7 @@ import Link from "next/link";
 import getRepos from "@/app/api/getRepos";
 import { DataProps } from "@/app/api/getRepos";
 import TheModal from "./StudyModal";
-import { FaCopyright } from "react-icons/fa";
-import {
-  BiLogoTypescript,
-  BiLogoJavascript,
-  BiLogoPython,
-} from "react-icons/bi";
-import { DiHtml5, DiCode } from "react-icons/di";
-import { FaTerminal } from "react-icons/fa";
+import GithubCard from "./GithubCard";
 const Github = async () => {
   const repos = await getRepos();
   const filteredRepos = repos.filter((repo: DataProps) => {
@@ -32,71 +25,14 @@ const Github = async () => {
               {filteredRepo.map((repo: DataProps, index: any) => {
                 const shortenedCreatedAt = repo.created_at.slice(0, 10);
                 return (
-                  <a href={repo.html_url} target="_blank" key={index}>
-                    <div>
-                      <div className="dark:transparent cursor-pointer  mb-5 md:mb-0  h-[250px] flex flex-col ring-gray-600  ring-1  shadow-md p-7 ">
-                        <div className="h-[30%] flex align-center  gap-3">
-                          <div>
-                            {repo.language === "Python" ? (
-                              <BiLogoPython size={50} />
-                            ) : repo.language === "Shell" ? (
-                              <FaTerminal size={50} />
-                            ) : repo.language === "JavaScript" ? (
-                              <BiLogoJavascript size={50} />
-                            ) : repo.language === "TypeScript" ? (
-                              <BiLogoTypescript size={50} />
-                            ) : repo.language === "HTML" ? (
-                              <DiHtml5 size={50} />
-                            ) : repo.language === "C" ? (
-                              <FaCopyright size={50} />
-                            ) : (
-                              <DiCode size={50} />
-                            )}
-                          </div>
-                          <div className="flex flex-col">
-                            <div className="relative text-lg font-semibold ">
-                              <div>
-                                <span
-                                  className={`${
-                                    repo.language === "Python"
-                                      ? "underline-python"
-                                      : repo.language === "Shell"
-                                      ? "underline-shell"
-                                      : repo.language === "JavaScript"
-                                      ? "underline-javascript"
-                                      : repo.language === "TypeScript"
-                                      ? "underline-typescript"
-                                      : repo.language === "HTML"
-                                      ? "underline-html"
-                                      : repo.language === "C"
-                                      ? "underline-c"
-                                      : "highlight"
-                                  }`}
-                                >
-                                  {repo.language === "C"
-                                    ? "C Language"
-                                    : repo.language}
-                                </span>
-                              </div>
-                            </div>
-                            <p className="line-clamp-1 ">{repo.name}</p>
-                          </div>
-                        </div>
-                        <div className="h-[70%] pt-2 pl-1 text-sm flex gap-1 flex-col">
-                          <p className="text-gray-600 ">{"<h3>"}</p>
-                          <div className="flex gap-3 ">
-                            <div className="">
-                              <div className="h-full ml-4 border-l border-gray-600 border-solid "></div>
-                            </div>
-                            <p className="h-full line-clamp-3">
-                              {repo.description}
-                            </p>
-                          </div>
-                          <p className="text-gray-600 ">{"</h3>"}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
+                  <GithubCard
+                    height="250px"
+                    key={index}
+                    repoUrl={repo.html_url}
+                    repoDesc={repo.description}
+                    repoLang={repo.language}
+                    repoName={repo.name}
+                  />
                 );
               })}
               <div className="flex gap-4 px-3 py-6">
@@ -130,52 +66,3 @@ const Github = async () => {
 };
 
 export default Github;
-/*
-
-<>
-                   <div className="dark:transparent dark:bg-opacity-40 mb-5 md:mb-0  h-[200px] flex flex-col justify-between p-[16px]  rounded-md ring-1 ring-gray-800 dark:ring-0 shadow-md">
-                      <div className="h-[60%] ">
-                        <p className="pb-2 text-sm font-light text-blue-400">
-                          {repo.language}
-                        </p>
-                        <p className="pb-2 text-lg font-bold truncate">
-                          {repo.name}
-                        </p>
-                        <div className="">
-                          <div className="text-[10px] font-semibold mb-5  truncate  ">
-                            {repo.description}
-                          </div>
-                        </div>
-
-                        <hr className="bg-[#07284b] opacity-40" />
-                      </div>
-                      <div className="flex items-end justify-between">
-                        <div className="flex flex-col">
-                          <div className="flex gap-2">
-                            <Image
-                              src="/star1.png"
-                              height={2}
-                              width={18}
-                              alt="star"
-                            />
-                            <p className="text-sm font-light">
-                              {repo.stargazers_count}
-                            </p>
-                          </div>
-
-                          <p className="text-sm">{shortenedCreatedAt}</p>
-                        </div>
-                        <a href={repo.html_url} target="_blank">
-                          <button className="w-8 h-8 bg-[#07284b]  rounded-md flex justify-center items-center">
-                            <Image
-                              src="/arrow.png"
-                              width={20}
-                              height={20}
-                              alt="arrow"
-                            />
-                          </button>
-                        </a>
-                      </div>
-                    </div>
-                  </>
- */
